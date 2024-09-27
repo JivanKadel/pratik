13f25eafae55d2238c6db4df80e078ddf20155cf
Jivan Kadel <jivankadel256@gmail.com>
2024-09-27 11:01:52 +0545

    "Modified product card"

--- src/components/fe_carousel.jsx
+++ src/components/fe_carousel.jsx
@@ -1,29 +1,32 @@
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
-
+import '../styles/fe_carousel.css';
+import Pizza from '../assets/pizza.avif';
 export default function FeCarousel() {
 	return (
-		<Carousel>
-			<div>
+		<Carousel
+			autoPlay={true}
+			infiniteLoop={true}
+			showThumbs={false}
+			dynamicHeight={false}
+		>
+			<div className="fe_carousel">
 				<img
 					alt="Buff Momo"
 					src="https://plus.unsplash.com/premium_photo-1661600630493-a1dad6b9519b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8TW9tb3xlbnwwfDB8MHx8fDA%3D"
 				/>
 				<p className="legend">Buff MoMo</p>
 			</div>
-			<div>
-				<img
-					alt="Buff Momo"
-					src="https://plus.unsplash.com/premium_photo-1661600630493-a1dad6b9519b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8TW9tb3xlbnwwfDB8MHx8fDA%3D"
-				/>
-				<p className="legend">Buff MoMo</p>
+			<div className="fe_carousel">
+				<img alt="Pizza" src={Pizza} />
+				<p className="legend">Pizza</p>
 			</div>
-			<div>
+			<div className="fe_carousel">
 				<img
-					alt="Buff Momo"
-					src="https://plus.unsplash.com/premium_photo-1661600630493-a1dad6b9519b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8TW9tb3xlbnwwfDB8MHx8fDA%3D"
+					alt="Steak"
+					src="https://images.unsplash.com/photo-1432139555190-58524dae6a55?q=80&w=2676&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 				/>
-				<p className="legend">Buff MoMo</p>
+				<p className="legend">Steak</p>
 			</div>
 		</Carousel>
 	);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,11 @@ import FeCarousel from '../components/fe_carousel';
 
 export default function Home() {
 	return (
-		<div>
-			{/* <h1
-				style={{
-					textAlign: 'center',
-					fontSize: '5rem'
-				}}
-			>
-				Home page fr
-			</h1> */}
+		<div
+		// style={{
+		// 	marginTop: '2rem'
+		// }}
+		>
 			<FeCarousel></FeCarousel>
 		</div>
 	);
